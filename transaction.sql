a054b742b7a35dd277f448e5cee3392e5e4db751
Marcelinoooo <146707307+Marcelinoooo@users.noreply.github.com>
2023-12-07 17:11:12 +0100

    transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -6,16 +6,16 @@ START TRANSACTION;
 
 -- Ajout d'un étudiant
 INSERT into student(email, id_house, name, year)
-VALUES ('harry.potter@hogwarts.com', 1, 'harry potter', 4);
+VALUES ('hermione.granger@hogwarts.com', 1, 'hermione granger', 4);
 
 -- Afficher l'étudiant ajouté précédemment
-SELECT * FROM student WHERE name= 'harry potter';
+SELECT * FROM student WHERE name= 'hermione granger';
 
 -- Annulation de l'ajout d'étudiant
 ROLLBACK;
 
 -- Verification du rollback
-SELECT * FROM student WHERE name= 'harry potter';
+SELECT * FROM student WHERE name= 'hermione granger';
 
 
 -- 4) Modification mutliple et commit
