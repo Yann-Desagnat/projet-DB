95b0212605893ff4e217d22f9d6cb95764777dfb
Marcelinoooo <146707307+Marcelinoooo@users.noreply.github.com>
2023-12-07 15:12:53 +0100

    transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -1,9 +1,12 @@
 -- Début de la transaction
-
 START TRANSACTION;
 
 -- Ajout d'un étudiant
 INSERT into student(email, id_house, name, year)
-VALUES ('harry.potter@hogwarts.com', 1, 'harry potter', '2025');
+VALUES ('harry.potter@hogwarts.com', 1, 'harry potter', 4);
 
+-- Afficher l'étudiant ajouté précédemment
+SELECT * FROM student WHERE name= 'harry potter';
+
+-- Validation de la transaction
 COMMIT;
