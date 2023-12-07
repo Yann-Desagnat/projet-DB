647f1e0f78cd3460f7a878088dd91dea05716198
Marcelinoooo <146707307+Marcelinoooo@users.noreply.github.com>
2023-12-07 16:56:24 +0100

    transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -41,3 +41,17 @@ COMMIT;
 SELECT * from student WHERE name = 'harry potter';
 SELECT * FROM registration WHERE id_student = 9; 
 
+
+-- 5) Transaction avec erreur et rollback
+
+USE hogwarts;
+
+-- Début de la transaction
+START TRANSACTION;
+
+-- Essais de modifications pour arriver à un echec
+INSERT INTO student(email, id_house, name, year)
+VALUES ('seamus.finnigan@hogwarts.com', 1, 'seamus finnegan', 4),('harry.potter@hogwarts.com', 3, 'harry bonheur', 3);
+
+-- Annulation de toutes les opérations faites précédemment
+COMMIT;
