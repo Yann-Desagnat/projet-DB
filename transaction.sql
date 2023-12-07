3532df3ae6d0461a93bd7529abb212761966b48e
Marcelinoooo <146707307+Marcelinoooo@users.noreply.github.com>
2023-12-07 14:59:07 +0100

    transaction.sql

--- transaction.sql
+++ transaction.sql
@@ -1,1 +1,9 @@
+-- Début de la transaction
 
+START TRANSACTION;
+
+-- Ajout d'un étudiant
+INSERT into student(email, id_house, name, year)
+VALUES ('harry.potter@hogwarts.com', 'gryffindor', 'harry potter', '2025');
+
+COMMIT;
