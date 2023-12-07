c354180811a7f07ba56fb960425c9d9c03d8e1f8
Yann-Desagnat <103132907+Yann-Desagnat@users.noreply.github.com>
2023-12-07 14:12:53 +0100

    Add files via upload

---
+++ normalisation.sql
@@ -0,0 +1,38 @@
+create database hogwarts;
+use hogwarts;
+
+create table if not exists house(
+id int primary key auto_increment not null,
+name enum("gryffindor", "slytherin", "ravenclaw", "hufflepuff")
+);
+
+create table if not exists student(
+	id int primary key auto_increment not null,
+    name varchar(100) not null,
+    email varchar(150),
+    year tinyint not null,
+    id_house int not null,
+    foreign key (id_house) references house(id)
+);
+
+
+create table if not exists prefet (
+	id int primary key auto_increment not null,
+    id_student int not null,
+    id_house int not null,
+    foreign key (id_house) references house(id),
+    foreign key (id_student) references student(id)
+);
+
+create table if not exists course (
+	id int primary key auto_increment not null,
+    name varchar(100) not null
+);
+
+create table if not exists registration (
+	id int primary key auto_increment not null,
+    id_student int not null,
+    id_course int not null,
+    foreign key (id_course) references course(id),
+    foreign key (id_student) references student(id)
+    );
